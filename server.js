a7e544e15cdcda1a6234151e5cba8e709dec5576
Adnan <adnan@Adnans-MacBook-Pro.local>
2024-03-20 15:40:28 +0100

    .env file

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB)
+  .then(() => console.log('Connected successfully to MongoDB'))
+  .catch((error) => console.error('Error connecting to MongoDB:', error));
 
 // console.log(process.env);
 const port = process.env.PORT || 3000;
