12cb8bb3ab639f6af980ef99dbc00cbe9ac32084
Adnan <adnan@Adnans-MacBook-Pro.local>
2024-03-28 06:22:30 +0100

    some security features added like rate limiting, http headers data sanitizing, parameter populizing

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -7,12 +12,47 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-//1) Middlewares
+//1) Global Middlewares
+// Set Security Http headers
+app.use(helmet());
+
+// Development Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); // log every request to the console
 }
-app.use(express.json());
+
+// Limit requests from an IP
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000, // 1 hour window
+  message: 'Too many requests from this IP, please try again in an hour',
+});
+app.use('/api', limiter);
+
+// Body Parser Middleware, Reading Data from Body into req.body
+app.use(express.json({ limit: '10kb' }));
 // app.use(express.static(`${__dirname}/public`));  // to access static files from the public folder
+
+// Sanitize Data against no-SQL Injection
+app.use(mongoSanitize());
+
+// Sanitize Data against XSS
+app.use(xss());
+
+// Prevent HTTP Parameter Pollution
+app.use(
+  hpp({
+    wwhitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'price',
+      'difficulty',
+    ],
+  }),
+);
+// test middleware
 app.use((req, res, next) => {
   req.reqTime = new Date().toISOString();
   next();
