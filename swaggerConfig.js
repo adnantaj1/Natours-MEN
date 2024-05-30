4ce32ff92de208a9b7149bcc97a65a1267336610
Adnan <adnan@Adnans-MacBook-Pro.local>
2024-05-30 13:56:45 +0200

    swagger link

--- swaggerConfig.js
+++ swaggerConfig.js
@@ -38,6 +38,8 @@ const options = {
       {
         url: 'http://localhost:3000/api/v1',
         description: 'Development server',
+        url: https://https://natours-men-2.onrender.com
+        description: Live server
       },
     ],
     paths: {
